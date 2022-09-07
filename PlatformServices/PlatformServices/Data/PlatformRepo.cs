97c956dee4787ec634ddb6fb17cccd3f8343f88f
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-09-07 14:32:38 +0300

    class PlatformRepo
    this class realized IPlatformRepo interface

---
+++ PlatformServices/PlatformServices/Data/PlatformRepo.cs
@@ -0,0 +1,38 @@
+﻿using PlatformServices.Models;
+
+namespace PlatformServices.Data
+{
+    public class PlatformRepo : IPlatformRepo
+    {
+        private readonly AppDbContext _context;
+
+        public PlatformRepo(AppDbContext context)
+        {
+            _context = context;
+
+        }
+        public void CreatePlatform(Platform plat)
+        {
+            if(plat == null)
+            {
+                throw new ArgumentNullException(nameof(plat));
+            }
+            _context.Platfroms.Add(plat);
+        }
+
+        public Platform GetPlatformById(int id)
+        {
+            return _context.Platfroms.FirstOrDefault(p => p.Id == id);
+        }
+
+        public IEnumerable<Platform> GetPlatforms()
+        {
+            return _context.Platfroms.ToList();
+        }
+
+        public bool SaveChanges()
+        {
+            return (_context.SaveChanges() >= 0);
+        }
+    }
+}
