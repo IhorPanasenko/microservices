4065f48282e48e1b1652dcb311923da7d11686c9
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-09-07 14:24:30 +0300

    Add IPlatfromRepo interface

---
+++ PlatformServices/PlatformServices/Data/IPlatformRepo.cs
@@ -0,0 +1,12 @@
+﻿using PlatformServices.Models;
+
+namespace PlatformServices.Data
+{
+    public interface IPlatformRepo
+    {
+        bool SaveChanges();
+        IEnumerable<Platform> GetPlatforms();
+        Platform GetPlatformById(int id);
+        void CreatePlatform(Platform plat);
+    }
+}
