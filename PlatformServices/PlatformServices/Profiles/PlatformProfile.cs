a05f5644adf97868a4fd4b648143fe46637a7e45
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-09-08 15:00:19 +0300

    Add mapping

---
+++ PlatformServices/PlatformServices/Profiles/PlatformProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using PlatformServices.Dtos;
+using PlatformServices.Models;
+
+namespace PlatformServices.Profiles
+{
+    public class PlatformProfile:Profile
+    {
+        public PlatformProfile()
+        {
+            // Source --> Target
+            CreateMap<Platform, PlatformReadDto>();
+            CreateMap<PlatformCreateDto, Platform>();
+        }
+
+    }
+}
