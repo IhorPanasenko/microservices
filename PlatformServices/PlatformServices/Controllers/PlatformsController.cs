6c714523606d518ec2e0e931e2be4e8563170586
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-09-09 11:14:46 +0300

    Create Controller

---
+++ PlatformServices/PlatformServices/Controllers/PlatformsController.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformServices.Data;
+
+namespace PlatformServices.Controllers
+{
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController(IPlatformRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+    }
+}
